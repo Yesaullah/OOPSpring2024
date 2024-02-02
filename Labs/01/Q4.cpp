2be89a758525e3a777c10bfd6c8e9dc1c8f00939
Yesaullah <142867724+Yesaullah@users.noreply.github.com>
2024-02-02 19:32:17 +0500

    Update Q4.cpp

--- Labs/01/Q4.cpp
+++ Labs/01/Q4.cpp
@@ -2,6 +2,18 @@
 
 using namespace std;
 
+int difference (int ind1, int ind2){ // function which will check which one of the two vertical lines is smaller
+	if (ind1 < ind2){
+		return ind1;
+	}
+	else if (ind2 < ind1){
+		return ind2;
+	}
+	else {
+		return ind1; // when both of them are equal
+	}
+}
+
 int main(){
 	int n; // variable for the size of the array
 	cout << "--> Enter the number of vertical lines: ";
@@ -9,39 +21,34 @@ int main(){
 	cout << "\n";
 	int height[n]; // declaring array of size n
 	// taking inputs for the array
-	int i; // loop controlling variable
+	int i, j; // loop controlling variables
 	for (i=0; i<n; i++){
 		cout << "--> Enter the height of the vertical line " << i + 1 << ": ";
 		cin >> height[i];
 	}
-	//max will store the largest value and max1 will store the second largest value
-	int max = height[0];
-	int max1 = height[0];
-	// variables to store the values of the x-coordinates
-	int a = 1;
-	int b = 1; 
-	
-	for (i=1; i<n; i++){
-		if (height[i] > max){
-			max = height[i];
-			a = i+1;
+	int temp; // temporary variable
+	int area = 0;
+	for (i=0; i<n; i++){
+		for(j=0; j<n; j++){
+			if (i != j){ // they must not be equal in order to have some area 
+				if (i>j){
+					temp = (i-j) * difference(height[i], height[j]);
+					if (temp > area){
+						area = temp;
+					}
+				}
+				else {
+					temp = (j-i) * difference(height[i], height[j]);
+					if (temp > area){
+						area = temp;
+					}
+				}
+			}
+			else {
+				continue;
+			}
 		}
 	}
-	for (i=1; i<n; i++){
-		if (height[i] > max1 && height[i] < max){
-			max1 = height[i];
-			b = i+1;
-		}
-	}
-	int diff; //variable to store the separation over x-axis
-	if (a > b){
-		diff = a - b;
-	}
-	else {
-		diff = b - a;
-	}
-	int area; //variable to store the max area of water
-	area = max1 * diff;
 	
 	cout << "\n--> The max area of water is: " << area;
 }
