8b67d69929ef2ea1b2713f58bb6008729ec3cdb4
Yesaullah <142867724+Yesaullah@users.noreply.github.com>
2024-02-15 15:21:22 +0500

    Update Q1.cpp

--- Labs/04/Q1.cpp
+++ Labs/04/Q1.cpp
@@ -31,7 +31,9 @@ class Book{
 		}
 		//method to update the pages read
 		void Read(){
-			PagesRead++; // incrementing the number of pages read whenever thi method is called in the main function
+			if(PagesRead<PageCount){
+				PagesRead++; // incrementing the number of pages read whenever thi method is called in the main function
+			}
 			if(PagesRead == PageCount){
 				cout<<"You have finished the book."<<endl;
 			}
@@ -40,11 +42,20 @@ class Book{
 				cout<<"You have read "<<PagesRead<<" pages. "<<remaining<<" pages are still remaining to be read."<<endl;
 			}
 		}
+		void display(){
+			cout<<"Book Name: "<< name<<endl;
+			cout<<"Author: "<<author<<endl;
+			cout<<"ISBN: "<<ISBN<<endl;
+			cout<<"Total pages: "<<PageCount<<endl;
+		}
 };
 
 int main(){
 	string title, writer, num;
 	int pages, pagesread;
-	Book b1("Harry Potter", "J. K. Rowling", "987654321", 500, 250);
+	Book b1("Harry Potter", "J. K. Rowling", "987654321", 500, 249);
+	b1.display();
 	b1.Read();
+	
+	return 0;
 }
