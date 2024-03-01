054c0026e9cc442c644fad9585746e96a837284b
Yesaullah <142867724+Yesaullah@users.noreply.github.com>
2024-03-01 22:07:35 +0500

    Update Task1.cpp

--- Labs/05/Task1.cpp
+++ Labs/05/Task1.cpp
@@ -9,19 +9,17 @@ class Square{
     private:
     float sidelength;
     float area;
-    float allareas;
+    static float allareas;
     public:
     //default constructor
-    Square() {}
+    Square() {
+        sidelength = 0;
+        area = 0;
+    }
     //parameterized constructor
     Square(float sidelength){
         this->sidelength = sidelength;
         area = 0;
-        allareas = 0;
-    }
-    void CalculateArea(){
-        area = (this->sidelength) * (this->sidelength);
-        allareas += area;
     }
     // getters and setters 
     void setlength(float sidelength){
@@ -30,7 +28,7 @@ class Square{
     void setArea(float area){
         this->area = area;
     }
-    void setallareas(float allareas){
+    void setAllareas(float allareas){
         this->allareas = allareas;
     }
     float getlength(){
@@ -42,4 +40,30 @@ class Square{
     float getallareas(){
         return allareas;
     }
+    // function to calculate the area
+    void CalculateArea(){
+        area = (sidelength)*(sidelength);
+        allareas += area;
+    }
 };
+// initialization of the static variable 
+float Square::allareas = 0;
+
+int main(){
+    Square sq1(4); // object  creation using parameterized constructor
+    sq1.CalculateArea(); // calling member function which calculates the area.
+    cout<<"Area of the square = "<<sq1.getarea()<<endl;
+    cout<<"The total area of all squares is = "<<sq1.getallareas()<<endl;
+
+    // objects creation using default constructor
+    Square sq2;
+
+    float s;
+    cout<<"\nEnter the side length for the square: ";
+    cin>>s;
+    sq2.setlength(s);
+    sq2.CalculateArea();
+    cout<<"Area of this square is equal to "<<sq2.getarea()<<endl;
+
+    cout<<"\nThe total area of all the squares is = "<<sq2.getallareas()<<endl;
+}
